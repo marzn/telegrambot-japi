693ba1eda87807ef91afbc0d7d14023761114efc
danielketterer <daniel.ketterer+gitlabcom@dipdi.de>
2015-10-05 15:52:13 +0200

    testbvot angelegt

--- src/test/java/de/vivistra/telegrambot/client/TestBot.java
+++ src/test/java/de/vivistra/telegrambot/client/TestBot.java
@@ -3,10 +3,20 @@ package de.vivistra.telegrambot.client;
 import org.junit.Test;
 
 public class TestBot {
+<<<<<<< HEAD
 
 	@Test
 	public void test() {
 
 	}
 
+=======
+	/*
+	 * was was ist die Klasse Bot: - Singleton Https-Verbindung bauen - post
+	 * methode - close methode
+	 * 
+	 * was soll die Klasse können: - https verbindung aufbauen zur api - http
+	 * methode post anbieten
+	 */
+>>>>>>> testbvot angelegt
 }
